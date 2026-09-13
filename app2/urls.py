bdb23724d42949de011b13d025c355cf8ec4dafb
benjaaacode <benjaminvivancosnchez@gmail.com>
2026-09-13 01:11:04 -0300

    Se agregan redirecciones para las rutas base de app1 y app2

--- app2/urls.py
+++ app2/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='vista1/')),
     path('vista1/', views.vista1, name='vista1'),
     path('vista2/', views.vista2, name='vista2'),
 ]
--- evaluacion/urls.py
+++ evaluacion/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
     path('app2/', include('app2.urls')),
+    path('', RedirectView.as_view(url='app1/', permanent=True)),
 ]
